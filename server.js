3e0a480aae6dce5d4e751393952b66ef8d259e89
quinz <quinzel0325@gmail.com>
2020-04-22 18:29:57 -0500

    hello world api

---
+++ server.js
@@ -0,0 +1,16 @@
+let express = require('express')
+let routes = require('./routes/routes')
+
+//added for path
+let path = require('path')
+
+let app = express()
+app.use('/api', routes)
+
+//added for deployment
+app.use(express.static(path.join(__dirname, 'hello-vue', 'dist')))
+
+
+let server = app.listen(process.env.PORT || 3000, function () {
+        console.log('Express server running on port', server.address().port)
+})
